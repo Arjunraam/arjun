2a7520f49a90e078e2d5460d376a1f29e0205bc8
dhanvarshini22 <dhanvarshini22@gmail.com>
2025-05-24 08:46:43 +0530

    inital commit

---
+++ test/src/App.js
@@ -0,0 +1,6 @@
+
+import './App.css';
+import Table from './Table.js';
+import Image from './Image.js';
+
+export default App;
---
+++ test/src/Image.js
@@ -0,0 +1,15 @@
+import './Image.css';
+function Image() {
+    return (
+        <div className="Image">
+            <div class="card">
+                <img src="lawn.jpg" className="Img" />
+                <div class="card-text">
+                    <h1>Ready</h1>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+export default Image;
